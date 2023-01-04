c0c6d5b1adb68375ed106b8b6c80001b961af3cf
Alan <freedom480@gmail.com>
2023-01-03 18:27:27 -0800

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
-# python-sqlite-to-markdown
-Python app to export SQLite query results to markdown.
+# Exporting SQL Results to Markdown
+This repo contains a Python app to export the results of a SQLite query to a markdown file.
+
+This software may be used/updated under the terms of the **MIT license**.  
+
+## Requirements
+* [Python 3](https://www.python.org/downloads/)
+
+## Usage
+The app is expecting the following command line args:
+
+|Arg|Description|
+|---|---|
+|1<sup>st</sup>|Path to SQLite database file|
+|2<sup>nd</sup>|Path to SQL query file|
+|3<sup>rd</sup> (Optional)|Directory where markdown file will be created|
+
+Note: The markdown file created will have the same filename as the input sql file, but with the extension changed to **md**.
+
+Example:
+```sh
+$ python3 export-to-markdown.py sample.db query.sql
+```
+
+In this case, a *query.md* file with the results will be created in the current directory - the same directory as the *query.sql* file.
+
+Example with optional export directory:
+```sh
+$ python3 export-to-markdown.py sample.db query.sql export-data
+```
+
+In this case, a *query.md* file with the results will be created in the *export-data* directory.
+
+## Markdown File
+The contents of the markdown file will depend on the query file and the results it produces. The markdown file will contain the following:
+
+1. A heading based on query filename.
+2. If the query file contains any SQL comments at the top of the file, these will be exported to the markdown file.
+3. The query results will be converted to a markdown table, with the field names being used as the table headings.
+
+An example of the output format is shown below:
+
+```
+# query-filename-without-ext
+
+Optional comment(s) from start of query file.
+
+|ID|Value|
+|:-:|---|
+|1|Value A|
+|2|Value B|
+
+```
+
+Note: Numeric fields will be center aligned, normal text will be left aligned.
---
+++ export-to-markdown.py
@@ -0,0 +1,225 @@
+#################################################
+# App to export SQLite query results to markdown.
+#
+# Alan Barr (GitHub: freedom35)
+# Jan 2023
+#################################################
+import sqlite3
+import sys
+import os
+
+#######################################
+# Main method
+#######################################
+def main():
+    try:
+        # Check for expected arguments
+        if len(sys.argv) < 3:
+            display_help()
+            return
+
+        # Get paths - first arg is file name
+        dbFile = sys.argv[1]
+        sqlFile = sys.argv[2]
+
+        # Check database exists otherwise SQLite lib will create an empty one
+        if not os.path.isfile(dbFile):
+            print('Database not found: {}'.format(dbFile))
+            return
+
+        if not os.path.isfile(sqlFile):
+            print('SQL file not found: {}'.format(sqlFile))
+            return
+
+        # Separate directory/filename
+        tmp = os.path.split(sqlFile)
+        sqlFileDir = tmp[0]
+        sqlFileWithExt = tmp[1]
+
+        # Separate filename/ext
+        tmp = os.path.splitext(sqlFileWithExt)
+        sqlFileWithoutExt = tmp[0]
+        sqlFileExt = tmp[1]
+
+        # Export to same directory as SQL file if optional dir not specified
+        exportDir = sys.argv[3] if len(sys.argv) > 3 else sqlFileDir
+
+        # File ext for output file
+        MARKDOWN_EXT = '.md'
+        
+        # Check input file is not markdown or will be overwritten
+        if sqlFileDir == exportDir and sqlFileExt == MARKDOWN_EXT:
+            print('SQL file ({}) cannot have same ext as markdown file ({}) - SQL file would be overwritten!'.format(sqlFileWithExt, MARKDOWN_EXT))
+            return
+        
+        # Get contents of SQL file
+        sql = read_query(sqlFile)
+
+        # Query DB
+        conn = sqlite3.connect(dbFile)
+
+        results = get_results(conn, sql)
+
+        # Check for SQL comments in file
+        comments = get_header_comments(sql)
+
+        # Format results as markdown
+        markdown = create_markdown(sqlFileWithoutExt, comments, results)
+
+        # Check to create dir for output file
+        if not os.path.exists(exportDir):
+            os.makedirs(exportDir)
+
+        # Get output path
+        exportFile = sqlFileWithoutExt + MARKDOWN_EXT
+        exportPath = os.path.join(exportDir, exportFile)
+
+        export_to_file(exportPath, markdown)
+
+        # Tidy up
+        conn.close()
+
+        print('Export complete: {}'.format(exportFile))
+
+    except Exception as e:
+        print('Error: {}'.format(str(e)))
+
+
+#######################################
+# Help
+#######################################
+def display_help():
+    pythonFile = os.path.basename(__file__)
+    print('Expecting <database path> <sql path> [optional: <export dir>]')
+    print('Example:')
+    print(' python3 {} sample.db query.sql'.format(pythonFile))
+    print('or')
+    print(' python3 {} sample.db query.sql export-dir'.format(pythonFile))
+
+
+#######################################
+# Read SQL file contents
+#######################################
+def read_query(path):
+    sql = ''
+
+    # Open file as read-only
+    with open(path, 'r') as f:
+        sql = f.read()
+
+    return sql
+
+
+#######################################
+# Get any header comments from sql
+#######################################
+def get_header_comments(sql):
+    comments = []
+
+    # Check start of file for comments
+    for line in sql.splitlines():
+        if line.startswith('--'):
+            # Remove comment marker and any spaces
+            comments.append(line.lstrip('- ').rstrip(' \r\n'))
+        else:
+            break
+
+    return comments
+
+
+#######################################
+# Get results from database
+#######################################
+def get_results(conn, sql):
+    # Get database cursor
+    cur = conn.cursor()
+
+    # Execute SQL statement
+    cur.execute(sql)
+  
+    # Get list of column names for query
+    names = list(map(lambda x: x[0], cur.description))
+
+    # Get results into a table (list of lists)
+    selectedResults = cur.fetchall()
+
+    # Return tuple
+    return (names, selectedResults)
+
+
+#######################################
+# Create markdown content
+#######################################
+def create_markdown(title, body, table):
+    lines = []
+    
+    # Title
+    lines.append('# {}'.format(title))
+    lines.append('')
+
+    # Optional body
+    if len(body) > 0:
+        for s in body:
+            lines.append(s)
+
+        # Add extra line if added body
+        lines.append('')
+
+    # Table
+    if len(table) < 2:
+        return lines
+
+    fields = table[0]
+    rows = table[1]
+
+    # Check for row data
+    if len(rows) == 0:
+        return lines
+
+    # Build table markdown
+    headings = '|'
+    alignment = '|'
+
+    # Table headings
+    for field in fields:
+        # Add field name, replace underscores
+        headings += field.replace('_', ' ') + '|'
+
+    lines.append(headings)
+
+    # Align table based on data type
+    # (center align if numeric)
+    for val in rows[0]:
+        if isinstance(val, int) or isinstance(val, float):
+            alignment += ':-:|'
+        else:
+            alignment += '---|'
+
+    lines.append(alignment)
+
+    # Row data
+    for row in rows:
+        lines.append('|{}|'.format('|'.join([str(i) for i in row])))
+
+    return lines
+
+
+#######################################
+# Export to file
+#######################################
+def export_to_file(exportPath, markdown):
+    # Create file for writing
+    with open(exportPath, 'w') as f:
+        # Insert newline char between each item
+        # (Otherwise list will be concatenated into one line)
+        separateLines = map(lambda x: f"{x}\n", markdown)
+
+        # Write all lines to file
+        f.writelines(separateLines)
+
+
+#######################################
+# Local entrypoint
+#######################################
+if __name__ == "__main__":
+    main()
